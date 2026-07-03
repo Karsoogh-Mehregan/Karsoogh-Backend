1392d34639e663b98675fd55b8b9105b3e07c154
AliAlmasiZ <ali1385almasi@gmail.com>
2026-07-03 12:55:20 +0330

    fix: add page_size for pagination

--- core/settings.py
+++ core/settings.py
@@ -239,7 +239,7 @@ REST_FRAMEWORK = {
         "rest_framework.throttling.UserRateThrottle",
     ],
     "DEFAULT_THROTTLE_RATES": {"anon": "300/day", "user": "1000/day"},
-    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "DEFAULT_PAGINATION_CLASS": "core.pagination.CustomPageNumberPagination",
     "PAGE_SIZE": 50,
     "DEFAULT_FILTER_BACKENDS": [
         "django_filters.rest_framework.DjangoFilterBackend",
